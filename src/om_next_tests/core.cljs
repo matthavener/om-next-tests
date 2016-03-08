72764e788f9bb2f0b697dbfca054985efaccb7cd
Chris McFarlen <cmcfarlen@apple.com>
2016-03-08 10:51:17 -0600

    add tree links test.

--- src/om_next_tests/core.cljs
+++ src/om_next_tests/core.cljs
@@ -3,6 +3,7 @@
    [om.next :as om :refer-macros [defui]]
    [sablono.core :as sab :refer-macros [html]]
    [om-next-tests.merge]
+   [om-next-tests.tree-links]
    [devcards.core :as dc])
   (:require-macros
    [devcards.core :as dc :refer [defcard deftest]]))
